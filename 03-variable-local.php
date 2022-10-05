667f67a93900f726fea012aa722eddfa6947f4b1
marshmalocode <gustinmelvin6@gmail.com>
2022-10-05 15:14:58 +0200

    pm

--- 03-variable-local.php
+++ 03-variable-local.php
@@ -7,6 +7,58 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>variable local</h1>
+<h1>Variables locales</h1>
+    <p>En PHP, les variables sont représentées par un signe dollar "$" suivi du nom de la variable. Le nom est sensible à la casse.<br>
+Les noms de variables suivent les mêmes règles de nommage que les autres entités PHP. Un nom de variable valide doit commencer par une lettre ou un souligné (_) mais on va l'éviter car les superglobales l'utilisent, et l'OO (orienté objet aussi), suivi de lettres, chiffres ou soulignés.</p>
+<p>On va éviter les caractères spéciaux, même si certains sont valides. il faut absolument éviter les signes servant aux calculs (+-*/)</p>
+
+<h2>Utilité des variables</h2>
+<p>Elles servent à stocker des informations</p>
+<p>Elles peuvent être manipulées, affichées, sauvegardées, détruites etc...</p>
+<h4>Mettez les variables valides (même les déconseillées) dans les balises PHP</h4>
+<pre>
+    $lulu = 5;
+    $lala = "yes";
+    $5main = 3;
+    $_____coucou = 7;
+    $bool = true;
+    $bool 2 = false;
+    $je-t-aime = "pas";
+    $ILoveYou = "me too";
+    $6-repos = 77;
+    $bon-homme = true;
+    $lkhjfgt5468sd58757 = 677;
+    $mabapta = $lulu;
+</pre>
+<hr>
+<?php
+    $lulu = 5;
+    echo '$lulu = ';
+    echo "$lulu <br>";
+
+    $lala = "yes";
+    echo '$lala = ';
+    echo "$lala <br>";
+
+    $_____coucou = 7;
+    echo '_____coucou = ';
+    echo "$_____coucou <br>";
+
+    $bool = true;
+    echo'$bool =';
+    echo"$bool <br>";
+
+    $ILoveYou = "me too";
+    echo 'ILoveYou =';
+    echo"$ILoveYou <br>";
+
+    $lkhjfgt5468sd58757 = 677;
+    echo '$lkhjfgt5468sd58757 =';
+    echo"$lkhjfgt5468sd58757 <br>";
+
+    $mabapta = $lulu;
+    echo '$mabapta =';
+    echo"$mabapta <br>";
+?>
 </body>
 </html>
