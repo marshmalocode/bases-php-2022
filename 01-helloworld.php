4f41aab13d9f4be00564ac37059f1151968fe73d
marshmalocode <gustinmelvin6@gmail.com>
2022-10-05 11:48:33 +0200

    testphp

---
+++ 01-helloworld.php
@@ -0,0 +1,20 @@
+<?php
+//commentaire sur une ligne
+
+// on peu mettre des balises PHP n'importe ou dans une page html
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World</title>
+</head>
+<body>
+    <?php //ouverture de PHP
+    //echo pour affiché la ligne echo"text";
+    echo "<h1>Hello World</h1>";
+   //fermeture de PHP ?> 
+</body>
+</html>
